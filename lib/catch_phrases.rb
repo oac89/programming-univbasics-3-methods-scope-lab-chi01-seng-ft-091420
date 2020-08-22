178f0d2489f61af05ccf154251e92e6c6ac9b4bc
Oscar A Castro <oacastro2@gmail.com>
2020-08-22 14:09:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,3 +9,14 @@ def toadstool
   phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This"
+  puts phrase
+end
+
+def all_phrases 
+  puts mario 
+  puts toadstool
+  puts link
+end
